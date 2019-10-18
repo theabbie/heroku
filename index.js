0ffd16be8ee9fcc89ee395f8b65b2e31a17ef775
abhishek <abhishek7gg7@gmail.com>
2019-10-18 14:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var app = require('express')();
 var axios = require("axios")
 
 app.get("/*", function(req,res) {
+res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  })
+setInterval(function() {
+res.write("hi");
+},10000)
 var list = [];
 for (i=0;i<100;i++) {list[i]=axios("https://screenshot-v2.now.sh/https:/www.buzzfeed.com/abhishek7gg7/how-boring-are-you-biydj0dk4j")}
 function go() {
