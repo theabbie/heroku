033d31e13dafdf1b07fc140ea175d336b521c25e
abhishek <abhishek7gg7@gmail.com>
2019-10-18 13:57:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var app = require('express')();
+var axios = require("axios")
 
 app.get("/*", function(req,res) {
-
+var list = [];
+for (i=0;i<100;i++) {list[i]=axios("https://screenshot-v2.now.sh/https:/www.buzzfeed.com/abhishek7gg7/how-boring-are-you-biydj0dk4j")}
+function go() {
+Promise.all(list).then(function() {go()})
+}
+go()
 })
 
 app.listen(process.env.PORT);
